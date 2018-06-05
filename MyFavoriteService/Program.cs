44ca7cc9ca6b9a202a499ddb30a4b498d5092373
ITHUSET\fredrik.aslin <fredrik.aslin@revide.se>
2018-06-05 17:05:34 +0200

    Initial commit

--- MyFavoriteService/Program.cs
+++ MyFavoriteService/Program.cs
@@ -10,6 +10,8 @@ namespace MyFavoriteService
     {
         static void Main(string[] args)
         {
+            Console.Write("hejsan Service");
+            Console.ReadKey();
         }
     }
 }

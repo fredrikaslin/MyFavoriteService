d22fc94b4395fce8b9384870f5209db97617f41e
ITHUSET\fredrik.aslin <fredrik.aslin@revide.se>
2018-11-09 16:23:36 +0100

    add signalR

--- MyFavoriteService/Program.cs
+++ MyFavoriteService/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +11,30 @@ namespace MyFavoriteService
     {
         static void Main(string[] args)
         {
-            RunDelegateTest();
-
+            //RunDelegateTest1();
+            //RunExpression();
+            //DelegateTest2v2();
+            DelegateTest2v1();
         }
 
-        public static void RunDelegateTest()
+        //expressions
+        #region
+        private static void RunExpression()
+        {
+            Expression<Func<string, string, string>> StringJoinExpr = (str1, str2) => string.Concat(str1, str2);
+
+            var result = StringJoinExpr.Compile()("Smith", "Jones");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+
+        #endregion
+
+
+        //delegates
+        #region 
+        public static void RunDelegateTest1()
         {
             NumberChanger nc1 = new NumberChanger(AddNum);
             NumberChanger nc2 = new NumberChanger(MultNum);
@@ -47,6 +67,33 @@ namespace MyFavoriteService
             return num;
         }
 
+        /*--------------------------------------------------------------------------------------------------*/
+
+        delegate int anonymousDel(int i, int j);
+        public static void DelegateTest2v1()
+        {
+            anonymousDel getBigInteger = (x, y) => { if (x > y) return x; else return y; };
+            Console.WriteLine(getBigInteger(10, 15));
+            Console.ReadKey();
+        }
+
+        public static void DelegateTest2v2()
+        {
+            Func<int, int, int> getBigInteger = (x, y) => { if (x > y) return x; else return y; };
+            Console.WriteLine(getBigInteger(10, 15));
+            Console.ReadKey();
+        }
+
+
+        delegate void doSomething();
+        public static void DelegateTest3()
+        {
+            doSomething IamVoid = () => { Console.WriteLine("Hello there! I take nothing and return nothing"); };
+            IamVoid();
+            Console.ReadKey();
+        }
+
+        #endregion  
 
     }
 }
